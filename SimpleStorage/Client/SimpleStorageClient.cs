98daf7e38041b96032283bd04f8edb25a5aec51e
fakefeik <feikfake1@gmail.com>
2015-04-10 23:34:20 +0500

    Task3Tests passed

--- SimpleStorage/Client/SimpleStorageClient.cs
+++ SimpleStorage/Client/SimpleStorageClient.cs
@@ -9,17 +9,20 @@ namespace Client
     public class SimpleStorageClient : ISimpleStorageClient
     {
         private readonly IEnumerable<string> endpoints;
+        private readonly CoordinatorClient coordinatorClient;
 
         public SimpleStorageClient(params string[] endpoints)
         {
             if (endpoints == null || !endpoints.Any())
                 throw new ArgumentException("Empty endpoints!", "endpoints");
             this.endpoints = endpoints;
+            coordinatorClient = new CoordinatorClient("http://127.0.0.1:17000/");
         }
 
         public void Put(string id, Value value)
         {
-            var putUri = endpoints.First() + "api/values/" + id;
+            var index = coordinatorClient.Get(id);
+            var putUri = endpoints.ElementAt(index) + "api/values/" + id;
             using (var client = new HttpClient())
             using (var response = client.PutAsJsonAsync(putUri, value).Result)
                 response.EnsureSuccessStatusCode();
@@ -27,7 +30,8 @@ namespace Client
 
         public Value Get(string id)
         {
-            var requestUri = endpoints.First() + "api/values/" + id;
+            var index = coordinatorClient.Get(id);
+            var requestUri = endpoints.ElementAt(index) + "api/values/" + id;
             using (var client = new HttpClient())
             using (var response = client.GetAsync(requestUri).Result)
             {
---
+++ SimpleStorage/start.bat
@@ -0,0 +1,4 @@
+@echo off
+
+Coordinator\bin\Debug\Coordinator.exe -p 17000 -c 3
+SimpleStorage\bin\Debug\runAllShards.bat
